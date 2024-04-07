6311fbd4b3668bd020d6eb0942a1bbbbbf627398
Vinicio Jacome <vjacomeg@my.centennialcollege.ca>
2024-04-06 23:02:35 -0400

    Add project files.

---
+++ ConsumeArti/Program.cs
@@ -0,0 +1,12 @@
+﻿using UtilityLibraries;
+
+namespace ConsumeArti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringLibrary.StartsWithUpper("Test"));
+        }
+    }
+}
